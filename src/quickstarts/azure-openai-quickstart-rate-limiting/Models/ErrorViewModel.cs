d5abb1256ff74ca6c99ec69e097db22b3494d93a
Nick Pinheiro <nickpinheiro@outlook.com>
2024-09-05 02:16:06 -0400

    Remove unnecessary files and code

--- src/quickstarts/azure-openai-quickstart-rate-limiting/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace azure_openai_quickstart_rate_limiting.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
