0c6bf57be839ba8cf3823150c9a6ae5350b5df38
Nick Pinheiro <nickpinheiro@outlook.com>
2024-10-01 15:45:44 -0400

    Add new project for Azure OpenAI image generation
    Introduce `azure-openai-quickstart-generate-images` project to the solution.
    Configure it to use .NET SDK targeting `net8.0` framework.
    Create `azure-openai-quickstart-generate-images.csproj` with necessary properties and package references.
    Add `Program.cs` to generate images using Azure OpenAI service.

---
+++ src/quickstarts/azure-openai-quickstart-generate-images/Program.cs
@@ -0,0 +1,23 @@
+﻿// Install the .NET library via NuGet: dotnet add package Azure.AI.OpenAI --prerelease
+using Azure.AI.OpenAI;
+using OpenAI.Images;
+using System.ClientModel;
+using static System.Environment;
+
+string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+string key = GetEnvironmentVariable("AZURE_OPENAI_KEY");
+
+ApiKeyCredential credential = new ApiKeyCredential(key);
+AzureOpenAIClient azureClient = new AzureOpenAIClient(new Uri(endpoint), credential);
+ImageClient client = azureClient.GetImageClient("dall-e-3");
+
+ClientResult<GeneratedImage> imageResult = await client.GenerateImageAsync("An abstract painting of the seattle skyline featuring the space needle and mount rainier", new()
+{
+	Quality = GeneratedImageQuality.Standard,
+	Size = GeneratedImageSize.W1024xH1024,
+	ResponseFormat = GeneratedImageFormat.Uri
+});
+
+// Image Generations responses provide URLs you can use to retrieve requested images
+GeneratedImage image = imageResult.Value;
+Console.WriteLine($"Image URL: {image.ImageUri}");
